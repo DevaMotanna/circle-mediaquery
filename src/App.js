12f1256444d1b5794db5ab5f2ceb4f080eef3e8e
DevaMotanna <125969042+DevaMotanna@users.noreply.github.com>
2024-01-18 21:43:35 -0800

    Count Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Count from "./Count";
 import { Welcome } from "./Welcome";
 
 function App() {
   return (
     <div>
       <Welcome name={"HTML"} age={30} />
+      <Count />
     </div>
   );
 }
